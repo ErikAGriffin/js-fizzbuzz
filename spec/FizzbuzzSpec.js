1455478961095b0c0d407b1f2603ef3ca58c1d92
Erik Griffin <griffin.erik@gmail.com>
2015-03-03 13:40:35 +0000

    Completed FizzBuzz

--- spec/FizzbuzzSpec.js
+++ spec/FizzbuzzSpec.js
@@ -12,6 +12,10 @@ describe('Fizzbuzz', function() {
       expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
     });
 
+    it('divided by 5', function() {
+      expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
+    });
+
   }); // end describe 'Knows a number'
 
   describe('Knows a number CANT be', function() {
@@ -20,9 +24,28 @@ describe('Fizzbuzz', function() {
       expect(fizzbuzz.isDivisibleByThree(5)).toBe(false);
     });
 
-
+    it('divided by 5', function() {
+      expect(fizzbuzz.isDivisibleByFive(1)).toBe(false);
+    });
 
   }); // end describe 'Knows a number CANT'
+
+  describe('Returns the string ', function() {
+
+    it("'Fizz' if the number is divisible by 3", function() {
+      expect(fizzbuzz.pass(3)).toEqual('Fizz');
+    });
+
+    it("'Buzz' if the number is divisible by 5", function(){
+      expect(fizzbuzz.pass(5)).toEqual('Buzz');
+    });
+
+    it("'FizzBuzz' if the number is divisible by 3 & 5", function() {
+      expect(fizzbuzz.pass(15)).toEqual('FizzBuzz');
+    });
+
+
+  }); // end describe 'Returns the string..'
 
 
 
--- src/Fizzbuzz.js
+++ src/Fizzbuzz.js
@@ -3,3 +3,18 @@ function Fizzbuzz() {};
 Fizzbuzz.prototype.isDivisibleByThree = function(number) {
   return number % 3 === 0
 };
+
+Fizzbuzz.prototype.isDivisibleByFive = function(number) {
+  return number % 5 === 0
+};
+
+Fizzbuzz.prototype.pass = function(number) {
+  var result = ""
+  if (this.isDivisibleByThree(number)) {
+   result += "Fizz"
+  }
+  if (this.isDivisibleByFive(number)) {
+   result += "Buzz"
+  }
+  return result
+};
