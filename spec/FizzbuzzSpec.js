a51ea173f8887ec6fa0eb829cc26cd7393bc0ed8
Erik Griffin <griffin.erik@gmail.com>
2015-03-03 11:57:39 +0000

    Initial Commit

---
+++ spec/FizzbuzzSpec.js
@@ -0,0 +1,23 @@
+describe('Fizzbuzz', function() {
+
+  var fizzbuzz;
+
+  beforeEach(function() {
+    fizzbuzz = new Fizzbuzz();
+  });
+
+
+  describe('Knows a number can be', function)() {
+
+    it('divided by 3', function() {
+      expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
+    });
+
+
+
+
+  };
+
+
+
+}); // end describe Fizzbuzz
