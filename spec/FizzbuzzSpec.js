b9c9b7b57e4dea943cccd65fa1b62d1441eea667
Erik Griffin <griffin.erik@gmail.com>
2015-03-03 12:24:57 +0000

    Numbers can and cant be divided by 3

--- spec/FizzbuzzSpec.js
+++ spec/FizzbuzzSpec.js
@@ -12,9 +12,17 @@ describe('Fizzbuzz', function() {
       expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
     });
 
-
-
   }); // end describe 'Knows a number'
+
+  describe('Knows a number CANT be', function() {
+
+    it('divided by 3', function() {
+      expect(fizzbuzz.isDivisibleByThree(5)).toBe(false);
+    });
+
+
+
+  }); // end describe 'Knows a number CANT'
 
 
 
